9a1a935c114b5d6c39cc318bfc34631cc0d8de7e
OBTITAN <emmanuel7ninsin@gmail.com>
2025-08-11 19:12:53 +0000

    Movie App creation start. Installation of Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -3,42 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
-const Card = ({title}) => {
-  const[count, setCount] = useState(0);
-
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect(()=>{
-    console.log(`${title} has been ${hasLiked}`)
-  }, [hasLiked])
-
-   useEffect(()=>{
-    console.log(`CARD RENDERED`)
-  }, [])
-
-  return(
-    <div className ="card" onClick={() => setCount(count + 1)}>
-      <h2>{title} <br/> {count || null}</h2>
-
-      <button onClick={() =>setHasLiked(!hasLiked)}>
-        {hasLiked? '❤️': '🤍'}
-      </button>
-    </div>
-  )
-}
-
 const App = () => {
 
-  return(
-
-      <div className='card-container'>
-        <Card title = "Star Wars" />
-        <Card title = "Avatar"/>
-        <Card title = "The Lion King" />
-      </div>
-
-  )
 }
 
 export default App
