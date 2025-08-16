a79e805c73c598c2f1d3ce6c4cbdc44cd91619f7
OBTITAN <emmanuel7ninsin@gmail.com>
2025-08-16 23:39:41 +0000

    Initializing and configuring Tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 const App = () => {
-
+    return(
+        <div>
+            <h1 class="text-3xl font-bold underline">
+            Hello world!
+            </h1>
+        </div>
+    )
 }
 
 export default App
