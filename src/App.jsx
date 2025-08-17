e3298d663ba18aacdfa63d695f2c4cebf8057c7e
OBTITAN <emmanuel7ninsin@gmail.com>
2025-08-17 18:14:55 +0000

    Renamed some assets, added image to app and created component folder with new Search component

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,10 @@ const App = () => {
     return(
         <main>
             <div className = "pattern" />
+
             <div className = "wrapper"> 
                 <header>
+                    <img src="./Hero-img.png" alt="Hero Banner"/>
                     <h1><span className="text-gradient">Movies </span>You'll Enjoy Without the Hassle</h1>
                 </header>
 
