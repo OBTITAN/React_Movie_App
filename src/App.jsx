e47539a81035618b9b47e71b6d8ed3a24a67c17f
OBTITAN <emmanuel7ninsin@gmail.com>
2025-08-25 18:04:40 +0000

    Addition of continue

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import search from './components/search'
+import Search from './components/Search.jsx'
 
 const App = () => {
+    const[searchTerm, setSearchTerm] = useState('');
+
     return(
         <main>
             <div className = "pattern" />
@@ -15,7 +17,9 @@ const App = () => {
                     <h1><span className="text-gradient">Movies </span>You'll Enjoy Without the Hassle</h1>
                 </header>
 
-                <search />
+                <Search searchTerm = {searchTerm} setSearchTerm = {setSearchTerm}/>
+
+                <h1 className='text-white'>{searchTerm}</h1>
             </div>
         </main>
     )
