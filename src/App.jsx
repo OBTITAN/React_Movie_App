147e30b00b3b9cb57f6a695639cece8f950b63ab
OBTITAN <emmanuel7ninsin@gmail.com>
2025-08-26 19:53:30 +0000

    addition of flowbite spinner and modification of index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import Search from './components/Search.jsx'
+import Spinner from './components/Spinner.jsx';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -71,13 +72,13 @@ const App = () => {
                 <h2>All Movies</h2>
 
                 {isLoading? (
-                    <p className ='text-white'>Loading....</p>
+                    <Spinner />
                 ) : errorMessage? (
                     <p className = 'text-red-500'>{errorMessage}</p>
                 ) : (
                     <ul>
                         {movieList.map((movie) => (
-                            <p className = "text-white">{movie.title}</p>
+                            <p key = {movie.id} className = "text-white">{movie.title}</p>
                         ))}
                     </ul>
                 )}           
