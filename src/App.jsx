664907f6ca043669effeebf4c95cfcd8b2b90a7b
OBTITAN <emmanuel7ninsin@gmail.com>
2025-08-28 00:53:37 +0000

    Setting up movie search

--- src/App.jsx
+++ src/App.jsx
@@ -22,12 +22,14 @@ const App = () => {
     const [movieList, setMovieList] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
 
-    const fetchMovies = async () => {
+    const fetchMovies = async (query = '') => {
         setIsLoading(true);
         setErrorMessage('');
 
         try{
-            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+            const endpoint = query
+            ?`${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+            :`${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
             
             const response = await fetch(endpoint, API_OPTIONS);
 
@@ -54,8 +56,8 @@ const App = () => {
 }
     
     useEffect(() => {
-        fetchMovies();
-        }, []);
+        fetchMovies(searchTerm);
+        }, [searchTerm]);
 
     return(
         <main>
@@ -73,7 +75,7 @@ const App = () => {
                 <h2 className="mt-[40px]">All Movies</h2>
 
                 {isLoading? (
-                    <Spinner />
+                    <Spinner/>
                 ) : errorMessage? (
                     <p className = 'text-red-500'>{errorMessage}</p>
                 ) : (
